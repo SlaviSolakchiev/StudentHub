32f7bd8d291b3a84f7cdb1530d02cd6e3f32179e
SlaviSolakchiev <78353852+SlaviSolakchiev@users.noreply.github.com>
2024-04-26 22:14:50 +0300

    Add ApplicationUserSeeder.cs

---
+++ Data/StudentHub.Data/Seeding/ApplicationUserSeeder.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using StudentHub.Common;
+using StudentHub.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentHub.Data.Seeding
+{
+    public class ApplicationUserSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+
+            await SeedUser(serviceProvider, new ApplicationUser() { UserName = "slaviadmin", Email = "slaviadmin@abv.bg" }, "slavi123", GlobalConstants.AdministratorRoleName);
+        }
+
+        private static async Task SeedUser(IServiceProvider serviceProvider, ApplicationUser user, string password,  string role)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            var result = await userManager.FindByNameAsync(user.UserName);
+
+            if (result == null)
+            {
+                await userManager.CreateAsync(user, password);
+
+                await userManager.AddToRoleAsync(user, role);
+            }
+        }
+    }
+}
