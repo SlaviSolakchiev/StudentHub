0ee37df04cd8e8fdff0ab154d12705174bf42e98
SlaviSolakchiev <78353852+SlaviSolakchiev@users.noreply.github.com>
2024-04-27 15:12:43 +0300

    Add dashboard Service for Courses

---
+++ Services/StudentHub.Services.Data/DashboardService.cs
@@ -0,0 +1,33 @@
+﻿using NPOI.SS.Formula.Functions;
+using StudentHub.Data.Common.Repositories;
+using StudentHub.Data.Models;
+using StudentHub.Services.Mapping;
+using StudentHub.Web.ViewModels.Administration.Dashboard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentHub.Services.Data
+{
+    public class DashboardService : IDashboardService
+    {
+        private readonly IRepository<Course> coursesRepo;
+
+        public DashboardService(IRepository<Course> coursesRepo)
+        {
+            this.coursesRepo = coursesRepo;
+        }
+
+        public IEnumerable<T> GetAllCourses<T>()
+        {
+            var list = this.coursesRepo.AllAsNoTracking()
+     .OrderBy(x => x.Id)
+     .To<T>()
+     .ToList();
+
+            return list;
+        }
+    }
+}
---
+++ Services/StudentHub.Services.Data/IDashboardService.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentHub.Services.Data
+{
+    using System.Collections.Generic;
+    using NPOI.SS.Formula.Functions;
+    using StudentHub.Web.ViewModels.Administration.Dashboard;
+
+    public interface IDashboardService
+    {
+        IEnumerable<T> GetAllCourses<T>();
+    }
+}
