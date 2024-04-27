2b1e67c705eb11604e3d960d33bda0d686fd1926
SlaviSolakchiev <78353852+SlaviSolakchiev@users.noreply.github.com>
2024-04-27 16:19:24 +0300

    Ad validation for Course Seeder

--- Data/StudentHub.Data/Seeding/CoursesSeeder.cs
+++ Data/StudentHub.Data/Seeding/CoursesSeeder.cs
@@ -3,20 +3,23 @@
     using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
-
+    using Microsoft.IdentityModel.Tokens;
     using StudentHub.Data.Models;
 
     public class CoursesSeeder : ISeeder
     {
         public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
         {
-            var courses = new List<Course>()
+            if (dbContext.Courses.IsNullOrEmpty())
             {
+                var courses = new List<Course>()
+                {
                 new Course() { Name = "Математика" },
                 new Course() { Name = "Български език и литература" },
-            };
+                };
 
-            await dbContext.Courses.AddRangeAsync(courses);
+                await dbContext.Courses.AddRangeAsync(courses);
+            }
         }
     }
 }
